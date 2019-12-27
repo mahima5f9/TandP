d21be0b802af3f422a80aaed6aca860135bfc5d7
Mahima <16k61a05f9@gmail.com>
2019-12-27 06:38:57 -0700

    Commit

---
+++ app/src/main/java/com/example/afinal/OTPActivity.java
@@ -0,0 +1,13 @@
+package com.example.afinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+    }
+}
